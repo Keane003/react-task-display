9205b1ec9275241de54499a62a695e24920e7b22
Keane003 <lx00391@innovaccion.mx>
2023-01-17 12:01:21 -0600

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,20 @@
+
+
+export const tasks = [
+    {
+        id: 0,
+        title: 'Primer tarea',
+        description: 'Debo bañar al gato',
+    },
+    {
+        id: 1,
+        title: 'Segunda tarea',
+        description: 'Debo bañar al perro',
+    },
+    {
+        id: 2,
+        title: 'Tercera tarea',
+        description: 'Comer',
+    },
+
+];
